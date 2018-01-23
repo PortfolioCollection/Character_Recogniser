c0e9664deedf3134aebdc6b6dc36c211f257e0c9
Tom <hiyoooo4@gmail.com>
2018-01-22 23:49:51 -0500

    Added a cropping function to tight fit an image
    It's tested to have a ~10% less accuracy than the uncropped images, therefore it is added but not used

--- -Averaged Approach-/Averaged_Tester.py
+++ -Averaged Approach-/Averaged_Tester.py
@@ -1,4 +1,6 @@
 import os
+from PIL import ImageOps
+from PIL import Image
 import sys
 import numpy as np
 sys.path.append('../_Core Functions_')
@@ -8,6 +10,7 @@ def test(answer_array,index,filename):
     FOLDER_NAME = "-Averaged Approach-"
 
     test_image = Extractor.getImage(filename)
+    ## test_image = crop_image(test_image)
     test = Extractor.ImageToMatrix(test_image)
     os.chdir('..')
     os.chdir(os.getcwd()+"/"+FOLDER_NAME+"/")
@@ -18,12 +21,15 @@ def test(answer_array,index,filename):
         trained_filename = str(x) + ".tif"    
         
         trained_image = Extractor.getImage(trained_filename)
-        trained = Extractor.ImageToMatrix(trained_image)            
+        ## trained_image = crop_image(trained_image)
+        ## trained_image = ImageOps.fit(trained_image, (len(test[0]),len(test)), Image.ANTIALIAS)
+        trained = Extractor.ImageToMatrix(trained_image)
+
         
         scores.append(matching_score(test, trained))
-    
+        
     guess = scores.index(min(scores))
-    confidence = (1 - min(scores)/(255*28*28)) * 100
+    # confidence = (1 - min(scores)/(255*28*28)) * 100
     os.chdir('..')
     os.chdir('..')
     os.chdir(os.getcwd()+"/test_images/")
@@ -65,8 +71,7 @@ def matching_score(test, digit):
                 invert_test = 255 - test[row][col]
                 invert_memory = 255 - digit[row][col]
                 score += abs(invert_memory - invert_test)
-    return score          
-    
+    return score    
 def run_test(num_tests=10000):
     STOP_AT = min(num_tests,10000)
     PERCENTILE = STOP_AT/100
@@ -95,8 +100,67 @@ def run_test(num_tests=10000):
 
     print(str(correct/index*100)+"% correct")
 
+def crop_image(image):
+    matrix = Extractor.ImageToMatrix(image)
+    # print(matrix)
+    top,bottom,left,right = get_image_bounds(matrix)
+    # print(get_image_bounds(matrix))
+    img = image.crop((left, top, right + 1, bottom + 1))
+       
+    return img  
+
+
+def get_image_bounds(matrix):
+    LIMIT = 200
+    # Top bound
+    top = 69
+    found = False
+    i = 0
+    while found == False:
+        for x in range(len(matrix[i])):
+            if matrix[i][x] < LIMIT:
+                top = i
+                found = True
+        i += 1
+    
+    # Bottom bound
+    bottom = 69
+    found = False
+    i = 27
+    while found == False:
+        for x in range(len(matrix[i])):
+            if matrix[i][x] < LIMIT:
+                bottom = i
+                found = True
+        i -= 1
+    
+    # Left bound
+    left = 69
+    found = False
+    i = 0
+    while found == False:
+        for x in range(len(matrix[i])):
+            if matrix[x][i] < LIMIT:
+                left = i
+                found = True
+        i += 1
+       
+    # Right bound
+    right = 69
+    found = False
+    i = 27
+    while found == False:
+        for x in range(len(matrix[i])):
+            if matrix[x][i] < LIMIT:
+                right = i
+                found = True
+        i -= 1
+
+    return (top, bottom, left, right)
+    
+
 if __name__ == "__main__":
-    run_test(500)
+    run_test(100)
 
     
 
