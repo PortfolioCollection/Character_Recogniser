d1e77bd865b11893ab0124949fe1595a15d38b22
Tom <hiyoooo4@gmail.com>
2018-01-23 21:43:59 -0500

    KNN tester implemented

--- -KNN Approach-/KNN_Tester.py
+++ -KNN Approach-/KNN_Tester.py
@@ -3,8 +3,45 @@ import sys
 import numpy as np
 sys.path.append('../_Core Functions_')
 import Extractor
+import KNN_Trainer
+import re
 
 def test(answer_array,index,filename):
+    FOLDER_NAME = "-KNN Approach-"
+    
+    
+    lr = KNN_Trainer.read_image(filename)
+    lean = KNN_Trainer.record_left_right(lr)
+    segments = KNN_Trainer.record_segment(lr)
+
+
+    os.chdir('..')
+    os.chdir(os.getcwd()+"/"+FOLDER_NAME+"/")
+    
+    neighbors = open("save.txt")
+
+    best_score = 100
+    optimal_number = -1
+    
+    for line in neighbors:
+        match = "line ([0-9]*): lean\(([0-9].[0-9]*)\) segment\(([0-9].[0-9]*)\) class\(([0-9])\)"
+        string = re.match(match, line)
+        train_line,train_lean,train_segments,train_number = string.group(1),string.group(2),string.group(3),string.group(4)
+        #print(train_line)
+        score = abs(lean-float(train_lean))+abs(segments-float(train_segments))
+        if score < best_score:
+            best_score = score
+            optimal_number = train_number
+    print("Score: "+str(score))
+    print("Optimal Number: "+str(optimal_number))
+                
+        
+
+    os.chdir('..')
+    os.chdir(os.getcwd()+"/test_images/")
+    print("Answer: "+str(answer_array[index]))
+    if answer_array[index] == optimal_number:
+        return 1
     return 0
     
 def run_test(num_tests=10000):
@@ -32,11 +69,12 @@ def run_test(num_tests=10000):
             percent += 1
         if index == STOP_AT:
             break
+        break #comment this out later
 
     print(str(correct/index*100)+"% correct")
 
 if __name__ == "__main__":
-    run_test(500)
+    run_test(1)
 
     
 
--- -KNN Approach-/KNN_Trainer.py
+++ -KNN Approach-/KNN_Trainer.py
@@ -10,12 +10,20 @@ def train_images():
 
     os.chdir('..')
     root = os.getcwd()
-    os.chdir(root +"/train_images_sorted/" + "0")
     points = []
-    for filename in os.listdir(os.getcwd()):
-        lr = read_image(filename)
-        points.append(record_left_right(lr))
-    plot(points)
+    numbers=[]
+    for number in range(2):
+        os.chdir(root +"/train_images_sorted/" + str(number))
+        for filename in os.listdir(os.getcwd()):
+            lr = read_image(filename)
+            lean = record_left_right(lr)
+            segments = record_segment(lr)
+            points.append((lean,segments))
+            numbers.append(number)
+        print("Done "+str(number))
+    #plot(points)
+    os.chdir(root+"/-KNN Approach-")
+    save_file(points,numbers, "save.txt")
                         
 def read_image(filename):
     image = Extractor.getImage(filename)
@@ -61,8 +69,21 @@ def record_left_right(lr):
                 right += 1
             total += 1
             
-    # return (left,right,total)
-    return (left/total, right/total)
+    #return (left,right,total)
+    return left/total+right/total
+
+def record_segment(lr):
+    one = 0
+    two = 0
+    total = len(lr)-1
+    for i in range(len(lr)-1):
+        seg = len(lr[i])
+        if seg == 2:
+            one+=1
+        elif seg == 4:
+            two+=1
+    #return (one, two, total)
+    return one/(3*total) +  2*two/(3*total)
 def plot(data):
     x = []
     y = []
@@ -74,6 +95,13 @@ def plot(data):
     plt.axis([0, 1, 0, 1])
     plt.show()
 
+def save_file(array, numbers, filename):
+    file=open(filename,"w")
+    line = 1
+    for element in array:
+        file.write("line "+str(line)+": lean("+str(element[0])+") segment("+str(element[1])+") class("+str(numbers[line-1])+")\n")
+        line+=1
+    file.close()
 #def record_segements():
 
 #def record_inc_dec():
@@ -81,4 +109,8 @@ def plot(data):
 if __name__ == "__main__":
     train_images()
     # print(lr)
-    # print(record_left_right(lr))
+    #os.chdir('..')
+    #root = os.getcwd()
+    #os.chdir(root +"/train_images_sorted/" + "1")
+    #lr = read_image("00004.tif")
+    #print(record_segment(lr))
